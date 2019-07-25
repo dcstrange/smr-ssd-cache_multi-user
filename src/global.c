e54230feadc0f2b39e64d04cc2a8e016e66b09b3
dcstrange <root@localhost.localdomain>
2019-07-25 19:42:30 +0800

    tmp

--- src/global.c
+++ src/global.c
@@ -26,7 +26,7 @@ blksize_t NBANDTables = 2621952;
 blksize_t SSD_SIZE = 4096;
 blksize_t BLKSZ = 4096;
 blkcnt_t  NZONES = 466034;/* size = 8TB */ //194180;    // NZONES * ZONESZ =
-blksize_t ZONESZ = 5000*4096;//18MB    // Unit: Byte.
+blksize_t ZONESZ = 5000*4096;//20MB    // Unit: Byte.
 
 char simu_smr_fifo_device[] = "/mnt/smr/pb";
 char simu_smr_smr_device[] = "/mnt/smr/smr";
@@ -63,3 +63,5 @@ char* PATH_LOG = "/home/outputs/logs";
 int PipeEnds_of_MAIN[HRC_PROCS_N];
 int PipeEnd_of_HRC;
 #endif
+
+
